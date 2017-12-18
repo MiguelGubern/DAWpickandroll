d93a3b0b0f3da1d0e11d4dc36e92e2034b107108
Santiago Miguel Gubern González <migui1995@hotmail.es>
2017-12-18 00:54:59 +0000

    Commit Prefinal

--- scripts.js
+++ scripts.js
@@ -1,51 +1,70 @@
-$(window).on("load",function() {
 
-    // $(window).scroll(function () {
-    //     //if you hard code, then use console
-    //     //.log to determine when you want the
-    //     //nav bar to stick.
-    //     console.log($(window).scrollTop())
-    //     if ($(window).scrollTop() > 175) {
-    //         $("#nav").addClass('navbar-fixed-top');
-    //     }
-    //     if ($(window).scrollTop() < 200) {
-    //         $("#nav").removeClass('navbar-fixed-top');
-    //     }
-    // });
-    toLast(1);
-    toLast(2);
-    toLast(3);
+function cargarSugerencias() {
+    // var insert = $("#añadirArticulos");
+    var html = "";
+    $.getJSON("../articulos.json", function (dataJson) {
+        $.each(dataJson.data, function (index, element) {
+            html += "<img src=\"" + element.img + "\" width=\"100%\">\n" +
+                "        <h4>" + element.articulo + "</h4>\n" +
+                "        <span>Fecha: " + element.fecha + "</span><hr/>";
+        });
+        console.log(html);
+        $("#añadirArticulos").append(html);
+    });
+}
 
-    // var d = new Date();
-    // var m = d.getMonth()+1;
-    // var text = d.getDate() + '-' + m + '-' + d.getFullYear()
-    // $("#date-resultados").text(text);
+function enviar() {
+    alert("Gracias por contactar con nosotros");
+}
 
-});
+var contador = 1;
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(28.0724719,-15.4515526),
+        zoom:18,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+function comentar() {
+    var comentario = $("textarea#comment").val();
+    $( ".media" ).append( "<div class=\"media-left\">\n" +
+        "<img src=\"../img/LogoP&Rnaranja.png\" class=\"media-object\" style=\"width:60px\">\n" +
+        "</div>\n" +
+        "<div class=\"media-body\">\n" +
+        "<h4 class=\"media-heading\">Tú</h4>\n" +
+        <!--<aqui deberia ponerse en vez de ese p con el lorem, poner el parrafo escrito por el usuario to bomba>-->
+        "<p id='añadir"+contador+"'></p>\n" +
+        "</div>\n" +
+        "<br>" );
+    $( "#añadir"+contador).append(comentario);
+    contador= contador+1;
+}
 
 function toLast(section) {
     $('#resultNext'+section).css("background-color", "");
-        $('#resultLast'+section).css("background-color", "gainsboro");
-        var html ="<table class=\"table\" id=\"tablePortada1\">\n" +
-            "                        <tr>\n" +
-            "                            <th>V</th>\n" +
-            "                            <th>-</th>\n" +
-            "                            <th>L</th>\n" +
-            "                        </tr>\n" +
-            "\n" +
-            "                            <tr>\n" +
-            "                                <td>F.C.B.</td>\n" +
-            "                                <th class=\"result-td\">96 - 97</th>\n" +
-            "                                <td>Herbalife GC</td>\n" +
-            "                            </tr>\n" +
-            "                            <tr>\n" +
-            "                                <td>CB Canarias</td>\n" +
-            "                                <th class=\"result-td\">79 - 66</th>\n" +
-            "                                <td>Baskonia</td>\n" +
-            "                            </tr>\n" +
-            "                    </table>";
-        $('#tablaResult'+section).html(html);
-    }
+    $('#resultLast'+section).css("background-color", "gainsboro");
+    var html ="<table class=\"table\" id=\"tablePortada1\">\n" +
+        "                        <tr>\n" +
+        "                            <th>V</th>\n" +
+        "                            <th>-</th>\n" +
+        "                            <th>L</th>\n" +
+        "                        </tr>\n" +
+        "\n" +
+        "                            <tr>\n" +
+        "                                <td>F.C.B.</td>\n" +
+        "                                <th class=\"result-td\">96 - 97</th>\n" +
+        "                                <td>Herbalife GC</td>\n" +
+        "                            </tr>\n" +
+        "                            <tr>\n" +
+        "                                <td>CB Canarias</td>\n" +
+        "                                <th class=\"result-td\">79 - 66</th>\n" +
+        "                                <td>Baskonia</td>\n" +
+        "                            </tr>\n" +
+        "                    </table>";
+    $('#tablaResult'+section).html(html);
+}
 
 
 
